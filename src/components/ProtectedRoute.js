92f2a530e52de44eed2aa2bdb19f9f0fa4096909
dansut24 <Danieljamessutton18@outlook.com>
2025-05-31 04:51:05 +0100

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,13 @@
+// src/components/ProtectedRoute.js
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = sessionStorage.getItem("isAuthenticated") === "true";
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  const user = localStorage.getItem("user");
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
