cfe0c8cab8ecea8b2fc62f2419fceaf470e6e973
dansut24 <Danieljamessutton18@outlook.com>
2025-06-04 06:14:41 +0100

    Create WidgetRegistry.js

---
+++ src/components/WidgetRegistry.js
@@ -0,0 +1,15 @@
+// components/widgetRegistry.js
+export const widgetRegistry = {
+  incidents: {
+    label: "My Incidents",
+    defaultLayout: { i: "incidents", x: 0, y: 0, w: 4, h: 3 },
+  },
+  requests: {
+    label: "Service Requests",
+    defaultLayout: { i: "requests", x: 4, y: 0, w: 4, h: 3 },
+  },
+  knowledge: {
+    label: "Knowledge Base",
+    defaultLayout: { i: "knowledge", x: 8, y: 0, w: 4, h: 3 },
+  },
+};
