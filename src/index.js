a55261c8e0757f5e1b8b4d9d6375d76113726277
dansut24 <Danieljamessutton18@outlook.com>
2025-06-05 20:33:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeModeProvider } from "./context/ThemeContext";
-import { AuthProvider } from "./context/AuthContext"; // ✅ Added import
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AuthProvider> {/* ✅ Wrap app in AuthProvider */}
-      <ThemeModeProvider>
-        <App />
-      </ThemeModeProvider>
-    </AuthProvider>
+    <Router> {/* ✅ Must be outermost wrapper */}
+      <AuthProvider>
+        <ThemeModeProvider>
+          <App />
+        </ThemeModeProvider>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );
