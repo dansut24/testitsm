beddf5eeff370300164cf76945304890551b24e7
dansut24 <Danieljamessutton18@outlook.com>
2025-06-05 23:13:52 +0100

    Update ProfileDrawer.js

--- src/components/ProfileDrawer.js
+++ src/components/ProfileDrawer.js
@@ -1,3 +1,5 @@
+// src/components/ProfileDrawer.js
+
 import React from "react";
 import {
   Drawer,
@@ -16,14 +18,16 @@ const ProfileDrawer = ({ open, onClose }) => {
   return (
     <Drawer anchor="right" open={open} onClose={onClose}>
       <Box sx={{ width: 300, p: 3, bgcolor: "background.paper" }}>
+        <Typography variant="h6" gutterBottom>
+          My Profile
+        </Typography>
+
+        <Divider sx={{ mb: 2 }} />
+
         <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
-          <Avatar sx={{ width: 56, height: 56 }}>
-            D
-          </Avatar>
+          <Avatar sx={{ width: 56, height: 56 }}>D</Avatar>
           <Box sx={{ ml: 2 }}>
-            <Typography variant="h6">
-              Dan Sutton
-            </Typography>
+            <Typography variant="h6">Dan Sutton</Typography>
             <Typography variant="body2" color="text.secondary">
               dan@hi5tech.co.uk
             </Typography>
