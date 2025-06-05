5dc3cc206a509f8fd649e956f037e6625aaaa393
dansut24 <Danieljamessutton18@outlook.com>
2025-06-05 20:57:43 +0100

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,9 +2,14 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import NotAuthorised from "../pages/NotAuthorised";
+import { useAuth } from "../context/AuthContext"; // ✅ Import context
 
 const ProtectedRoute = ({ allowedRoles, children }) => {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const { user, authLoading } = useAuth(); // ✅ Use context
+
+  if (authLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!user) {
     return <Navigate to="/login" replace />;
