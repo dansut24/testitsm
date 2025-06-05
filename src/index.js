24f52a78c154cbbb7fd144fb9b47f57cfcf6c467
dansut24 <Danieljamessutton18@outlook.com>
2025-06-05 20:04:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeModeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext"; // ✅ Added import
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ThemeModeProvider>
-      <App />
-    </ThemeModeProvider>
+    <AuthProvider> {/* ✅ Wrap app in AuthProvider */}
+      <ThemeModeProvider>
+        <App />
+      </ThemeModeProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
