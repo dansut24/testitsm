4db1638b54b7c4b98b952c8482f9a9d95c807894
dansut24 <Danieljamessutton18@outlook.com>
2025-06-11 22:27:44 +0100

    Update App.js

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MarketingLayout from "../layouts/MarketingLayout";
+import MarketingLayout from "./layouts/MarketingLayout";
 
 // Pages (now directly under main/pages)
 import Home from "./pages/Home";
