f716e4c63485921ac17af7e998d85780561e4d08
dansut24 <Danieljamessutton18@outlook.com>
2025-06-11 20:42:29 +0100

    Delete src/config directory

--- src/config/roles.js
+++
@@ -1,6 +0,0 @@
-// src/config/roles.js
-export const roles = {
-  Admin: ["view_all", "edit_all", "manage_users", "configure_settings"],
-  Agent: ["view_assigned", "edit_assigned", "add_comment"],
-  Requester: ["view_own", "raise_request"],
-};
