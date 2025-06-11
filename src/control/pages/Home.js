250d8bd87bbb3930547fcfbab1203a25d1c9b6af
dansut24 <Danieljamessutton18@outlook.com>
2025-06-11 07:03:06 +0100

    Create Home.js

---
+++ src/control/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Welcome to HI5Tech Control</h1>
+    <p>This portal allows you to manage devices remotely.</p>
+  </div>
+);
+
+export default Home;
