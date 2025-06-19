79cc7bd98a526ed33b699280e8400c8b6b8ef774
dansut24 <Danieljamessutton18@outlook.com>
2025-06-19 21:39:26 +0100

    Update ControlLayout.js

--- src/control/layouts/ControlLayout.js
+++ src/control/layouts/ControlLayout.js
@@ -1,15 +1,28 @@
+// src/control/components/Layout.js
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
 
 const ControlLayout = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   return (
-    <div style={{ display: "flex", minHeight: "100vh" }}>
+    <Box sx={{ display: "flex" }}>
       <Sidebar />
-      <main style={{ flex: 1, padding: "1rem" }}>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          ml: isMobile ? "80px" : "100px",
+          p: 3,
+          minHeight: "100vh",
+          bgcolor: "#f8f9fb",
+        }}
+      >
         <Outlet />
-      </main>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
