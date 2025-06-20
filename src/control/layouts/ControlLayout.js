568132c033317b03af4b273790b823b54a2782e9
dansut24 <Danieljamessutton18@outlook.com>
2025-06-20 20:20:28 +0100

    Update ControlLayout.js

--- src/control/layouts/ControlLayout.js
+++ src/control/layouts/ControlLayout.js
@@ -1,15 +1,24 @@
-// src/control/components/Layout.js
+// src/control/layouts/ControlLayout.js
 import React from "react";
-import Sidebar from "../components/Sidebar";
-import { Outlet, Navigate } from "react-router-dom";
-import { Box, useMediaQuery } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../common/context/AuthContext";
+import Sidebar from "../components/Sidebar";
+import { Box, useMediaQuery } from "@mui/material";
 import logo from "../../common/assets/865F7924-3016-4B89-8DF4-F881C33D72E6.png";
 
 const ControlLayout = () => {
+  const { user, authLoading } = useAuth();
   const isMobile = useMediaQuery("(max-width:600px)");
-  const { user } = useAuth();
 
+  if (authLoading) {
+    return (
+      <Box sx={{ p: 4 }}>
+        <p>Loading session...</p>
+      </Box>
+    );
+  }
+
+  // 🔒 Redirect unauthenticated users to login
   if (!user) {
     return <Navigate to="/login" replace />;
   }
