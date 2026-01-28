8531e7c671025fc77bfe340c61e26f76cce83194
dansut24 <Danieljamessutton18@outlook.com>
2026-01-28 21:22:51 +0000

    Update App.js

--- src/itsm/App.js
+++ src/itsm/App.js
@@ -1,6 +1,5 @@
 // src/itsm/App.js
 
-// Global styles for MDEditor
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 
@@ -13,13 +12,11 @@ import { ThemeModeProvider } from "./theme/ThemeContext";
 import { getCentralLoginUrl } from "../common/utils/portalUrl";
 import ExternalRedirect from "../common/components/ExternalRedirect";
 
-// Layout & Auth
 import Layout from "./components/Layout";
 import Loading from "./pages/Loading";
 import NotAuthorised from "./pages/NotAuthorised";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// Core Pages
 import Dashboard from "./pages/Dashboard";
 import Incidents from "./pages/Incidents";
 import ServiceRequests from "./pages/ServiceRequests";
@@ -32,17 +29,14 @@ import Approvals from "./pages/Approvals";
 import UserProfile from "./pages/UserProfile";
 import Settings from "./pages/Settings";
 import AdminSettings from "./pages/AdminSettings";
-
 import TenantOnboardingPage from "./pages/TenantOnboardingPage";
 
-// Create Pages
 import NewIncident from "./pages/NewIncident";
 import NewServiceRequest from "./pages/NewServiceRequest";
 import NewChange from "./pages/NewChange";
 import NewProblem from "./pages/NewProblem";
 import NewAsset from "./pages/NewAsset";
 
-// Detail Pages
 import IncidentDetail from "./pages/IncidentDetail";
 import ServiceRequestDetail from "./pages/ServiceRequestDetail";
 import ChangeDetail from "./pages/ChangeDetail";
@@ -50,14 +44,12 @@ import ProblemDetail from "./pages/ProblemDetail";
 import AssetDetail from "./pages/AssetDetail";
 import ArticleDetail from "./pages/ArticleDetail";
 
-// Other Features
 import Announcements from "./pages/Announcements";
 import WorkScheduler from "./pages/WorkScheduler";
 import NotFound from "./pages/NotFound";
 import SetPassword from "./pages/SetPassword";
 import NewTab from "./pages/NewTab";
 
-// Self-Service (optional within ITSM host)
 import SelfServiceLayout from "../selfservice/layouts/SelfServiceLayout";
 import SelfServiceHome from "../selfservice/pages/SelfServiceHome";
 import RaiseRequest from "../selfservice/pages/RaiseRequest";
@@ -67,8 +59,8 @@ import Checkout from "../selfservice/pages/Checkout";
 import SelfServiceConfirmation from "../selfservice/pages/CheckoutConfirmation";
 import SelfServiceKnowledgeBase from "../selfservice/pages/KnowledgeBase";
 
-// ✅ Shared Hi5 theme (matches portal + login)
-import { useHi5Theme } from "../common/ui/hi5Theme";
+// ✅ Shared Hi5 theme
+import { Hi5ThemeProvider, useHi5Theme } from "../common/ui/hi5Theme";
 import ThemeToggleIconButton from "../common/ui/ThemeToggleIconButton";
 
 function ITSMRoutes() {
@@ -103,19 +95,13 @@ function ITSMRoutes() {
 
   return (
     <Routes>
-      {/* If someone hits /login on -itsm host, hard redirect to central login */}
       <Route path="/login" element={<ExternalRedirect to={centralLogin} />} />
 
-      {/* These can stay on-module */}
       <Route path="/loading" element={<Loading />} />
       <Route path="/not-authorised" element={<NotAuthorised />} />
       <Route path="/set-password" element={<SetPassword />} />
 
-      {/* ITSM base */}
-      <Route
-        path="/"
-        element={isLoggedIn ? <Layout /> : <ExternalRedirect to={centralLogin} />}
-      >
+      <Route path="/" element={isLoggedIn ? <Layout /> : <ExternalRedirect to={centralLogin} />}>
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="incidents" element={<Incidents />} />
@@ -147,14 +133,12 @@ function ITSMRoutes() {
           }
         />
 
-        {/* Create */}
         <Route path="new-incident" element={<NewIncident />} />
         <Route path="new-service-request" element={<NewServiceRequest />} />
         <Route path="new-change" element={<NewChange />} />
         <Route path="new-problem" element={<NewProblem />} />
         <Route path="new-asset" element={<NewAsset />} />
 
-        {/* Detail */}
         <Route path="incidents/:id" element={<IncidentDetail />} />
         <Route path="service-requests/:id" element={<ServiceRequestDetail />} />
         <Route path="changes/:id" element={<ChangeDetail />} />
@@ -162,13 +146,11 @@ function ITSMRoutes() {
         <Route path="assets/:id" element={<AssetDetail />} />
         <Route path="knowledge-base/:id" element={<ArticleDetail />} />
 
-        {/* Other */}
         <Route path="announcements" element={<Announcements />} />
         <Route path="work-scheduler" element={<WorkScheduler />} />
         <Route path="newtab/:id" element={<NewTab />} />
       </Route>
 
-      {/* Self-Service (optional inside ITSM host) */}
       {isLoggedIn && (
         <Route path="/self-service" element={<SelfServiceLayout />}>
           <Route index element={<SelfServiceHome />} />
@@ -181,16 +163,12 @@ function ITSMRoutes() {
         </Route>
       )}
 
-      {/* Catch-all */}
-      <Route
-        path="*"
-        element={isLoggedIn ? <NotFound /> : <ExternalRedirect to={centralLogin} />}
-      />
+      <Route path="*" element={isLoggedIn ? <NotFound /> : <ExternalRedirect to={centralLogin} />} />
     </Routes>
   );
 }
 
-function App() {
+function ITSMInnerApp() {
   const { mode, tokens: t, toggleMode } = useHi5Theme();
 
   return (
@@ -207,7 +185,6 @@ function App() {
           position: "relative",
         }}
       >
-        {/* ✅ Floating theme toggle (doesn't touch ITSM Layout) */}
         <Box sx={{ position: "fixed", top: 14, right: 14, zIndex: 2000 }}>
           <ThemeToggleIconButton mode={mode} onToggle={toggleMode} t={t} />
         </Box>
@@ -218,4 +195,10 @@ function App() {
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Hi5ThemeProvider>
+      <ITSMInnerApp />
+    </Hi5ThemeProvider>
+  );
+}
